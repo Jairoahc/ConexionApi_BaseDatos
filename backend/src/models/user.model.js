d4c22ab9abfb5974236dde5fea652156bd0c33c6
jairoahc <jairohernandez010399@gmail.com>
2022-10-28 19:31:17 -0500

    Se agrego la validacion del email en la base de datos al momento de la creacion

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,11 +6,11 @@ const {Schema} = mongoose
 const userSchema =  new Schema(
     {
         /* firstName, lastName, email, password, isAdmin - true || false */
-        name: {type: String, require: true},
-        lastName: {type: String, require: true},
-        email: {type: String, require: true},
-        password: {type: String, require: true},
-        isAdmin: {type: Boolean, require: true},
+        name: {type: String, required: true},
+        lastName: {type: String, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true},
+        isAdmin: {type: Boolean, required: true},
 
     }, //las llaves de la coleccion.
     {
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -11,6 +11,20 @@ const User = require ("../models/user.model")
 //router.get("ruta", () => {})
 // La ruta post es para crear en bd
 router.post("/create-user", async (req, res) => {
+    //VALIDACION DEL EMAIL EN LA BASE DE DATOS PARA PERMITIR LA CREACION DEL USUARIO.
+    let {email} = req.body
+    /* let email = req.body.email */
+    //Pasar todo lo que se escriba en el email a minuscula para hacer la validacion
+    email = email.toLowerCase()
+    req.body.email = email
+    //Obtener los usuarios que tienen ese email en la base de datos
+    let userExists = await User.find({email: email})
+    //Validar si el email existe
+    if (userExists.length !== 0) {
+        return res.status(400).json({msg: "Este correo ya se encuentra registrado en la base de datos"})
+    }
+    //Guardar o enviar error de creacion 
+    
 
     const newUser = new User (req.body) //Para que la bd tome lo que se escriba en el body
     await newUser.save() //Para guardar un producto en la base de datos
